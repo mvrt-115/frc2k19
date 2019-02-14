86c86b390f9488369d87d1630dd949ad8deccb66
Hee Won Chung <heewonchung64@gmail.com>
2019-02-13 19:28:54 -0800

    idk if any of this will work

--- frc2k19/src/main/java/frc/robot/commands/ReturnStow.java
+++ frc2k19/src/main/java/frc/robot/commands/ReturnStow.java
@@ -10,11 +10,11 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
-public class Stow extends Command 
-{
+public class ReturnStow extends Command {
+
   public int position;
-  public Stow() 
-  {
+
+  public ReturnStow() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
     requires(Robot.ground);
@@ -23,37 +23,31 @@ public class Stow extends Command
 
   // Called just before this Command runs the first time
   @Override
-  protected void initialize() 
-  {
-    
+  protected void initialize() {
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
-  protected void execute() 
-  {
-    Robot.ground.stowIntake();
+  protected void execute() {
+    Robot.ground.returnStow();
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
-  protected boolean isFinished() 
-  {
-    return Robot.ground.stowing.getSelectedSensorPosition() == (position + 1024);
+  protected boolean isFinished() {
+    return Robot.ground.stowing.getSelectedSensorPosition() == (position - 1024);
   }
 
   // Called once after isFinished returns true
   @Override
-  protected void end() 
-  {
+  protected void end() {
     Robot.ground.stopStowing();
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
-  protected void interrupted() 
-  {
+  protected void interrupted() {
     Robot.ground.stopStowing();
   }
 }
